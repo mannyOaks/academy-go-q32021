fcaba3ed24e8cba609203e5371427bbd2702e267
Manuel E Robles <mannyrobh@gmail.com>
2021-10-06 17:35:31 -0700

    read_file functions commented

--- common/read_file.go
+++ common/read_file.go
@@ -6,6 +6,7 @@ import (
 	"os"
 )
 
+// ReadCsvFile - Returns data from `filename`
 func ReadCsvFile(filename string) ([][]string, error) {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -16,8 +17,10 @@ func ReadCsvFile(filename string) ([][]string, error) {
 	return reader.ReadAll()
 }
 
+// WriteToCsv - Writes `data` to `filename`
 func WriteToCsv(filename string, data [][]string) {
-	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, os.ModeAppend)
+	// 0644, 066, 0755
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0755)
 	if err != nil {
 		fmt.Println(err)
 	}
